61d5069bc6c7e6ecbcc7047d54e743a7ea097209
Roy <roylaw.w@gmail.com>
2018-03-17 07:37:07 -0400

    UPDATE: App.js has proper routers and landing page

--- draw-squad/src/client/App.js
+++ draw-squad/src/client/App.js
@@ -1,50 +1,22 @@
 // packages import
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Landing from './Landing';
 // files import
-import logo from './icons/logo.svg';
 import './styles/App.css';
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topics = () => (
-  <div>
-    <h2>Topics</h2>
-  </div>
-);
-
 export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/topics">Topics</Link>
-            </li>
-          </ul>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={Home}/>
-          <Route path="/topics" component={Home}/>
-        </div>
-      </Router>
-  );
-  }
+    render() {
+        return (
+          <Router>
+            <div>
+              <div className="title-bar">
+                <div className="title-bar-text">Draw.Squad</div>
+              </div>
+              <Route exact path="/" component={Landing}/>
+            </div>
+          </Router>
+        );
+    }
 }
