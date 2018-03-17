d87897bf46a236a5bf4595e263aa3d59237417e0
Roy <roylaw.w@gmail.com>
2018-03-16 21:11:39 -0400

    ADD: added examples of using router

--- draw-squad/src/client/App.js
+++ draw-squad/src/client/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
+// packages import
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+// files import
 import logo from './icons/logo.svg';
 import './styles/App.css';
 
-class App extends Component {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = () => (
+  <div>
+    <h2>Topics</h2>
+  </div>
+);
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={Home}/>
+          <Route path="/topics" component={Home}/>
+        </div>
+      </Router>
+  );
   }
 }
-
-export default App;
