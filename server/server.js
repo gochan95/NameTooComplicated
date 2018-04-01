635d8e2f48c54b7e55ef5f960990b3ddaeee4f9c
Gordon <gordon.chan95@live.ca>
2018-04-01 12:41:15 -0400

    test heroku deploy

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(flash());
 app.use(function (req, res, next) {
   console.log('starting server ..')
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://drawsquad.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
