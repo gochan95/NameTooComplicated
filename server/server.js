5a22da7511b20dece8fbb7b65ff9fabcee299c8c
roy <roylaw.w@gmail.com>
2018-04-01 19:11:14 -0400

    Allow access from origin

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(function (req, res, next) {
   console.log('starting server ..')
   // Website you wish to allow to connect
   // res.setHeader('Access-Control-Allow-Origin', 'http://ec2-18-219-119-149.us-east-2.compute.amazonaws.com:3000');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://drawsquad.herokuapp.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://drawsquad.me');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
