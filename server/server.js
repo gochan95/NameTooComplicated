38b78bb8c0d496c312a170a559a7e352e8d64783
roy <roylaw.w@gmail.com>
2018-04-01 23:42:36 -0400

    server

--- server/server.js
+++ server/server.js
@@ -37,13 +37,13 @@ app.use(function (req, res, next) {
   console.log('starting server ..')
   // Website you wish to allow to connect
   // res.setHeader('Access-Control-Allow-Origin', 'http://ec2-18-219-119-149.us-east-2.compute.amazonaws.com:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'http://drawsquad.me');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
